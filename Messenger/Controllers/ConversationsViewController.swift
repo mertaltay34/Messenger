3d54cc326507325fef851a678fe4e1f415fd1b3d
Mert <altayme19@itu.edu.tr>
2023-08-15 08:54:20 +0300

    Getting started

---
+++ Messenger/Controllers/ConversationsViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Messenger
+//
+//  Created by Mert Altay on 15.08.2023.
+//
+
+import UIKit
+
+class ConversationsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .blue
+    }
+
+
+}
+
